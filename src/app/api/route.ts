a5614f873c0372837793ce9ac13c6e4306bfa345
d3veduardo <eduardoaugustolimabueno@outlook.com.br>
2025-05-28 14:04:30 -0300

    Adicionado o try e catch na rota /api. Agora vai dar certo... :)

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,8 +3,13 @@ import { latestMusicListened } from "@libs/lanyard/latestMusicListened";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  latestMusicListened();
-  const data = await getLatestListenedMusic();
-  console.log("Dados lidos do db.json:", data);
-  return NextResponse.json(data, { status: 200 });
+  try {
+    const data = await getLatestListenedMusic();
+    console.log("Dados lidos do db.json:", data);
+    return NextResponse.json(data, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ message: "Erro interno!" }, { status: 500 })
+  }
+  
 }
