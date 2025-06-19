ad09d40c31237a1887b42ad0c3ed2a481a110d3d
d3veduardo <eduardoaugustolimabueno@outlook.com.br>
2025-06-19 00:43:54 -0300

    Atualizando função de salvar tokens do banco de dados

--- src/libs/firebase/setSpotifyTokens.ts
+++ src/libs/firebase/setSpotifyTokens.ts
@@ -2,7 +2,7 @@ import { db } from "./admin";
 import { SpotifyAuthPayload } from "@type/SpotifyApiTypes";
 
 export async function setSpotifyTokens(data: SpotifyAuthPayload): Promise<SpotifyAuthPayload> {
-    await db.ref("/").set(data);
+    await db.ref("/tokens").set(data);
     const snapshot = await db.ref("/tokens").get();
     return snapshot.val();
 }
