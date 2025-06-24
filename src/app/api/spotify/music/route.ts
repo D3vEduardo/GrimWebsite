77d73415ebf491c2fb4db1376b59bd0df6909ecd
d3veduardo <eduardoaugustolimabueno@outlook.com.br>
2025-06-24 17:14:01 -0300

    resolvendo o problema no refresh token da rota /music

--- src/app/api/spotify/music/route.ts
+++ src/app/api/spotify/music/route.ts
@@ -6,6 +6,7 @@ import {
   SpotifyAuthPayload,
 } from "@type/SpotifyApiTypes";
 import { NextRequest, NextResponse } from "next/server";
+import { env } from "@libs/zod/env";
 
 export async function GET(req: NextRequest) {
   try {
@@ -17,7 +18,7 @@ export async function GET(req: NextRequest) {
       );
 
     if (tokens.expiresAt <= Date.now()) {
-      const res = await fetch(new URL("", req.url), {
+      const res = await fetch(new URL("/api/spotify/auth/refresh", req.url), {
         method: "POST",
         body: JSON.stringify({
           refresh_token: tokens.refreshToken,
@@ -77,59 +78,6 @@ export async function GET(req: NextRequest) {
     return NextResponse.json([latestSong]);
   } catch (e) {
     console.error("API ROUTE /api/spotify/music (GET):", e);
-    return NextResponse.json(
-      { message: "Failed to fetch Spotify data" },
-      { status: 500 }
-    );
-  }
-}
-
-export async function POST() {
-  try {
-    const tokens = await getSpotifyTokens();
-    if (!tokens)
-      return NextResponse.json(
-        { message: "Spotify tokens not found" },
-        { status: 404 }
-      );
-    const res = await fetch(
-      "https://api.spotify.com/v1/me/player/recently-played?limit=10",
-      {
-        headers: {
-          Authorization: `Bearer ${tokens?.accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const data = (await res.json()) as RecentlyPlayedResponse;
-    if (!res.ok)
-      return NextResponse.json({
-        message: "Error on get spotify musics!",
-        body: data,
-      });
-
-    if (!data.items || data.items.length === 0)
-      return NextResponse.json(
-        {
-          message: "No recent music found",
-        },
-        { status: 404 }
-      );
-
-    const sortedSongs = data.items.sort(
-      (a, b) =>
-        new Date(b.played_at).getTime() - new Date(a.played_at).getTime()
-    );
-    const latestSong = sortedSongs[0];
-    await setLatestListenedMusic([latestSong]);
-
-    return NextResponse.json(
-      { message: "Latest listened music updated successfully" },
-      { status: 200 }
-    );
-  } catch (e) {
-    console.error("API ROUTE /api/spotify/music (POST):", e);
     return NextResponse.json(
       { message: "Failed to fetch Spotify data" },
       { status: 500 }
